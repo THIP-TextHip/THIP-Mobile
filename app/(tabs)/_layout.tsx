a659020bd6f970221bd08a6ee2f59fe7fca0ab74
holdn2 <yoocy01@gmail.com>
2025-12-19 21:28:04 +0900

    feat: 커스텀 탭바 구현
    todo: expo go 에서 (ios에서) border radius가 있는 곳에는 border color가 적용이 되지않는 이슈 해결

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { colors } from "@theme/token";
 import CustomTabBar from "@ui/CustomTabBar";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
+  // TODO: 탭바 아이콘 수정
   return (
     <Tabs
       screenOptions={{
@@ -15,24 +17,36 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "피드",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="group"
         options={{
           title: "모임",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="people" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
           title: "검색",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="my-page"
         options={{
           title: "내 정보",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
--- src/shared/ui/CustomTabBar.tsx
+++ src/shared/ui/CustomTabBar.tsx
@@ -2,60 +2,79 @@ import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { colors } from "@theme/token";
 import { AppText } from "@ui/AppText";
 import { Pressable, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-// 주입용 커스텀 탭바
 export default function CustomTabBar({
   state,
   descriptors,
   navigation,
 }: BottomTabBarProps) {
   return (
-    <View style={styles.TabBarContainer}>
-      {state.routes.map((route, index) => {
-        const focused = state.index === index;
-        const { options } = descriptors[route.key];
+    <SafeAreaView style={styles.outer}>
+      <View style={styles.tabBarContainer}>
+        {state.routes.map((route, index) => {
+          const focused = state.index === index;
+          const { options } = descriptors[route.key];
 
-        return (
-          <Pressable
-            key={route.key}
-            onPress={() => {
-              // 탭 라우팅은 React Navigation 방식으로 이동
-              navigation.navigate(route.name);
-            }}
-            style={{
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              padding: 12,
-              borderRadius: 12,
-            }}
-          >
-            <AppText style={{ color: "white", textAlign: "center" }}>
-              아이콘
-            </AppText>
-            <AppText
-              weight={focused ? "bold" : "regular"}
-              color={colors.white}
-              style={{ textAlign: "center" }}
+          const color = focused ? colors.purple.main : colors.grey[300];
+          const size = 24;
+
+          const icon =
+            typeof options.tabBarIcon === "function"
+              ? options.tabBarIcon({ focused, color, size })
+              : null;
+
+          return (
+            <Pressable
+              key={route.key}
+              onPress={() => navigation.navigate(route.name)}
+              style={styles.tabBarItemContainer}
             >
-              {options.title ?? route.name}
-            </AppText>
-          </Pressable>
-        );
-      })}
-    </View>
+              <View style={styles.tabBarItemIcon}>{icon}</View>
+              <AppText
+                weight="medium"
+                size="xs"
+                color={focused ? colors.purple.main : colors.grey[300]}
+                style={styles.tabBarItemTitle}
+              >
+                {options.title ?? route.name}
+              </AppText>
+            </Pressable>
+          );
+        })}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  TabBarContainer: {
-    display: "flex",
+  outer: {
+    backgroundColor: colors.black.main,
+  },
+
+  tabBarContainer: {
     flexDirection: "row",
     paddingHorizontal: 32,
     paddingVertical: 16,
-    borderTopColor: "#888888",
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.grey[300],
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12,
-    backgroundColor: "#121212",
+  },
+
+  tabBarItemContainer: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    gap: 5,
+  },
+
+  tabBarItemIcon: {
+    display: "flex",
+    alignSelf: "center",
+  },
+
+  tabBarItemTitle: {
+    textAlign: "center",
   },
 });
