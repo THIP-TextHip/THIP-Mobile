8889be22eb3ebff74aed33b6f77ff40f6034d890
holdn2 <yoocy01@gmail.com>
2025-12-23 22:16:21 +0900

    refactor: 이미지 화질 개선 및 크기 계산

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
 
 import { ThipSplash } from "@images/thip";
 import {
@@ -6,10 +6,15 @@ import {
   KakaoLoginButton,
 } from "./_components/login-button";
 
+// 디바이스 너비에 따라 스플래쉬 이미지 크기 계산
+const deviceWidth = Dimensions.get("window").width;
+const splashWidth = deviceWidth * 0.64;
+const splashHeight = splashWidth * 0.43;
+
 export default function Login() {
   return (
     <View style={styles.pageContainer}>
-      <Image source={ThipSplash} />
+      <Image source={ThipSplash} style={styles.splash} />
       <View style={styles.loginButtonWrapper}>
         <KakaoLoginButton />
         <GoogleLoginButton />
@@ -30,11 +35,16 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingTop: 100,
   },
+  splash: {
+    width: splashWidth,
+    height: splashHeight,
+  },
   loginButtonWrapper: {
     display: "flex",
     flexDirection: "column",
     gap: 20,
     paddingHorizontal: 20,
     width: "100%",
+    maxWidth: 600,
   },
 });
