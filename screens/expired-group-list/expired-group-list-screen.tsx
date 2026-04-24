8b4f4ae1c0319ce5edf2e7648de16ad9ad791eb1
holdn2 <yoocy01@gmail.com>
2026-04-24 22:46:36 +0900

    feature: 엠티뷰 추가

--- screens/expired-group-list/expired-group-list-screen.tsx
+++ screens/expired-group-list/expired-group-list-screen.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, useWindowDimensions, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { AppText, MyGroupCard } from "@shared/ui";
@@ -10,6 +10,7 @@ const NICKNAME = "띱테스트";
 
 export default function ExpiredGroupListScreen() {
   const { bottom } = useSafeAreaInsets();
+  const { height } = useWindowDimensions();
 
   const ListHeader = () => {
     return (
@@ -18,6 +19,30 @@ export default function ExpiredGroupListScreen() {
       </AppText>
     );
   };
+
+  const ExpiredGroupEmpty = () => {
+    return (
+      <View style={[styles.empty, { height: height - 300 }]}>
+        <AppText
+          weight="semibold"
+          size="lg"
+          color={colors.white}
+          lineHeight={24}
+        >
+          완료된 모임방이 없어요.
+        </AppText>
+        <AppText
+          weight="regular"
+          size="sm"
+          color={colors.grey[100]}
+          lineHeight={20}
+        >
+          모임방을 끝까지 완료해보세요.
+        </AppText>
+      </View>
+    );
+  };
+
   return (
     <FlatList
       contentContainerStyle={[styles.container, { paddingBottom: bottom + 20 }]}
@@ -33,6 +58,7 @@ export default function ExpiredGroupListScreen() {
           type={item.type}
         />
       )}
+      ListEmptyComponent={ExpiredGroupEmpty}
     />
   );
 }
@@ -41,5 +67,10 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     gap: 20,
+  },
+  empty: {
+    gap: 8,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
