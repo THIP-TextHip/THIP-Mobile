678458a1490823d6a1c17bcbc4cda9fbbcd8c414
holdn2 <yoocy01@gmail.com>
2026-05-10 13:33:57 +0900

    feature: 기록할 페이지 섹션 공통 컴포넌트로 구현

---
+++ screens/record-write/constants/index.ts
@@ -0,0 +1,6 @@
+export const DUMMY_RECORD_GROUP_STATE = {
+  totalBookPage: 600,
+  recentBookPage: 213,
+  isOverviewPossible: false,
+  roomId: 3,
+};
--- screens/record-write/record-write-screen.tsx
+++ screens/record-write/record-write-screen.tsx
@@ -1,17 +1,44 @@
-import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Keyboard, Pressable, StyleSheet, View } from "react-native";
+
+import { RecordPageSection } from "@shared/ui";
 
 import { RecordWriteHeader } from "./components";
+import { DUMMY_RECORD_GROUP_STATE } from "./constants";
 
 export default function RecordWriteScreen() {
+  const [recordPage, setRecordPage] = useState(
+    DUMMY_RECORD_GROUP_STATE.recentBookPage,
+  );
+  const [isOverview, setIsOverview] = useState(false);
+
+  const handleChangeOverview = () => {
+    setIsOverview((prev) => !prev);
+  };
+
   return (
-    <View style={styles.page}>
+    <Pressable style={styles.page} onPress={Keyboard.dismiss}>
       <RecordWriteHeader disabled={true} handleWriteRecord={() => {}} />
-    </View>
+      <View style={styles.content}>
+        <RecordPageSection
+          totalPage={DUMMY_RECORD_GROUP_STATE.totalBookPage}
+          isOverviewPossible={DUMMY_RECORD_GROUP_STATE.isOverviewPossible}
+          recordPage={recordPage}
+          isOverview={isOverview}
+          handleChangeRecordPage={setRecordPage}
+          handleChangeOverview={handleChangeOverview}
+        />
+      </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
+  },
+  content: {
+    paddingVertical: 32,
+    paddingHorizontal: 20,
   },
 });
